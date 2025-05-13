df45f6edfe6daedba56c5a28b0cff32f3dd330cf
Tharunkathulla <97688642+Tharunkathulla@users.noreply.github.com>
2025-05-12 21:33:47 -0500

    Add files via upload

---
+++ frontend/src/user/componets/UserList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './UserList.css';
+import Card from '../../shared/components/UIelement/Card';
+import UserItem from './UserItem';
+const UsersList=props =>
+{
+ if(props.items.length===0){
+    return(
+        <div className="center">
+            <Card>
+            <h2>No Users Found</h2>
+            </Card>
+        </div>
+    );
+    
+ }
+ return(
+    <ul className="users-list"> 
+        {props.items.map(user => <UserItem key={user.id} id={user.id} image={user.image} name={user.name} placeCount={user.places.length}/>)}
+    </ul>
+ );
+};
+export default UsersList;
